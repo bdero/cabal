90c80e359844a789c63a3448c24cb6660df6bd62
Brandon DeRosier <bdero@google.com>
2023-12-26 10:20:13 -0500

    Remove Bullet from deps.sh

--- deps.sh
+++ deps.sh
@@ -8,29 +8,6 @@ set -ex
 ENGINE_DIR=~/projects/flutter/engine/src
 IMPELLERC=$ENGINE_DIR/out/host_debug_unopt_arm64/impellerc
 
-function build_physics {
-  pushd $SCRIPT_DIR/deps/flutter_bullet
-  pushd native
-  cmake .
-  make -j 4
-  popd
-  dart run ffigen --config ffigen.yaml
-  popd
-
-  # The code that loads the native lib via `DynamicLibrary.open` is located in
-  # `deps/flutter_bullet/lib/flutter_bullet.dart`.
-  #
-  # Copy the compiled lib to gen, renaming it to the lookup name.
-  #
-  # The XCode project has a "Copy Files" build phase which grabs
-  # `gen/libflutter_bullet.dylib` and places it under `//Frameworks/native`
-  # within the app bundle.
-  mkdir -p $SCRIPT_DIR/build/macos/Build/Products/Debug/cabal.app/Contents/Frameworks/native
-  cp \
-    $SCRIPT_DIR/deps/flutter_bullet/native/libflutter_bullet.1.0.0.dylib \
-    $SCRIPT_DIR/gen/libflutter_bullet.dylib
-}
-
 function build_jolt {
   pushd $SCRIPT_DIR/plugins/ffi/jolt
   cmake .
@@ -57,7 +34,6 @@ function build_shaders {
     --shader-bundle=\{\"TextureFragment\":\ \{\"type\":\ \"fragment\",\ \"file\":\ \"$SCRIPT_DIR/shaders/flutter_gpu_texture.frag\"\},\ \"TextureVertex\":\ \{\"type\":\ \"vertex\",\ \"file\":\ \"$SCRIPT_DIR/shaders/flutter_gpu_texture.vert\"\}\}
 }
 
-# build_physics
 build_jolt
 build_v_hacd
 build_shaders
